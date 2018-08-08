dc0d1e135868115f7aed3b8a1cd957a3a61738f1
rain9525 <rain9525@users.noreply.github.com>
2018-08-08 17:51:10 +0900

    Add broadcast method.

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import tornado.ioloop
 import tornado.web
 import tornado.websocket
 
+
 # ルート
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
@@ -18,12 +19,41 @@ class PenInfo(tornado.web.RequestHandler):
     def post(self):
         pass
 
+# 描画情報ブロードキャスト
+class broadcastDrawInfo(tornado.websocket.WebSocketHandler):
+    # 接続者
+    waiters = set()
+
+#Todo:同一生成元(だったかな)のルールを無理やり回避しているのでちゃんと実装する
+    def check_origin(self, origin):
+        return True
+
+    def open(self, *args, **kwargs):
+        self.waiters.add(self)
+
+    def on_message(self, message):
+        print(message)
+        for waiter in self.waiters:
+            if waiter == self:
+                continue
+            waiter.write_message(message)
+
+    def on_close(self):
+        self.waiters.remove(self)
+
+
+class test(tornado.web.RequestHandler):
+    def get(self):
+        self.render('WStest.html')
+
 
 def make_app():
     return tornado.web.Application([
         # ルーティング
         (r"/", MainHandler),
-        (r"/penInfo", PenInfo)
+        (r"/penInfo", PenInfo),
+        (r'/soundOekaki', broadcastDrawInfo),
+        (r'/test', test)
     ])
 
 
