792caf3b8c90d9782685fa97ca059d0bca082684
rain9525 <rain9525@users.noreply.github.com>
2018-08-09 22:14:54 +0900

    Add some comments.

--- server.py
+++ server.py
@@ -12,36 +12,40 @@ class MainHandler(tornado.web.RequestHandler):
 
 
 # ペン情報取得
-class PenInfo(tornado.web.RequestHandler):
+class PenInfoHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("test")
 
     def post(self):
         pass
 
+
 # 描画情報ブロードキャスト
-class broadcastDrawInfo(tornado.websocket.WebSocketHandler):
+class broadcastDrawInfoHandler(tornado.websocket.WebSocketHandler):
     # 接続者
     waiters = set()
 
-#Todo:同一生成元(だったかな)のルールを無理やり回避しているのでちゃんと実装する
+    # すべての通信を受け入れる設定
     def check_origin(self, origin):
         return True
 
     def open(self, *args, **kwargs):
         self.waiters.add(self)
+        print(args)
+        print(kwargs)
+        # Todo:過去キャンバス送信
 
     def on_message(self, message):
         print(message)
         for waiter in self.waiters:
             if waiter == self:
                 continue
-            waiter.write_message(message)
+            waiter.write_message(message) #デフォルトでバイナリはfalseなのでbinaryを送るときは変える
 
     def on_close(self):
         self.waiters.remove(self)
 
-
+# テスト用ページ
 class test(tornado.web.RequestHandler):
     def get(self):
         self.render('WStest.html')
@@ -51,8 +55,8 @@ def make_app():
     return tornado.web.Application([
         # ルーティング
         (r"/", MainHandler),
-        (r"/penInfo", PenInfo),
-        (r'/soundOekaki', broadcastDrawInfo),
+        (r"/penInfo", PenInfoHandler),
+        (r'/soundOekaki', broadcastDrawInfoHandler),
         (r'/test', test)
     ])
 
