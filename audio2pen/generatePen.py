00cdf7be97a6d6083b3b6861a1fe2e18eea1d144
yan <yan@yan-MacBook.local>
2018-08-19 13:22:20 +0900

    スペクトラムからペン先生成追加

--- audio2pen/generatePen.py
+++ audio2pen/generatePen.py
@@ -3,15 +3,27 @@ import cv2
 import numpy as np
 import sys
 from acoustic_feature import extract
+from math import cos, sin, degrees, radians
+
+WIDTH,HEIGHT = 33,33  
+
+def powerSpectrum2Brush(ps):
+  img = np.ones((HEIGHT,WIDTH),dtype=np.uint8)*255
+  for t in range(0,360):
+    for r in range(0,int(WIDTH/2)):
+      x = int(r * cos(radians(t)) + WIDTH/2)
+      y = int(r * sin(radians(t)) + HEIGHT/2)
+      img[y,x] = 255 - int(ps[r*150]*255)
+
+  return img
 
 # feature => img
 def generateBrush(feature):
-    width,height = 32,32    
     PITCH_MAX = 2000
 
     db = max(feature["db"], -72) + 72 # [0:72]
 
-    img_filter = np.random.rand(width, height)
+    img_filter = np.random.rand(WIDTH, HEIGHT)
     thr = db/72 # [0,1.0]
     p = feature["pitch"]
     print(thr)
@@ -23,16 +35,16 @@ def generateBrush(feature):
     print(p)
 
     h = min(p / PITCH_MAX * 180,180)
-    s = np.ones( (width, height), dtype=np.uint8 ) * 255
-    v = np.ones( (width, height), dtype=np.uint8 ) * 200
+    s = np.ones( (WIDTH, HEIGHT), dtype=np.uint8 ) * 255
+    v = np.ones( (WIDTH, HEIGHT), dtype=np.uint8 ) * 200
     
-    for i in range(height):
-      for j in range(width):
+    for i in range(HEIGHT):
+      for j in range(WIDTH):
         if(img_filter[i,j] == 255):
           s[i,j] = 0
           v[i,j] = 255
 
-    img_hsv = np.ndarray( (width, height, 3), dtype=np.uint8 )
+    img_hsv = np.ndarray( (WIDTH, HEIGHT, 3), dtype=np.uint8 )
     for i, x in enumerate([h, s, v]):
       img_hsv[:,:,i] = x
     
@@ -43,13 +55,14 @@ def generateBrush(feature):
 if __name__ == '__main__':
     # argc, argv = len(sys.argv), sys.argv
     # if argc != 5:
-    #   print("usage: ./generatePen.py [width] [height] [file-name] [audio-name]")
+    #   print("usage: ./generatePen.py [WIDTH] [HEIGHT] [file-name] [audio-name]")
     #   quit()
-    argv = ["xxx", "32", "32", "pen.png", "c2.webm"]
+    argv = ["xxx", "32", "32", "pen_ps.png", "c2.webm"]
 
-    width, height = [int(x) for x in argv[1:3]]
+    # WIDTH, HEIGHT = [int(x) for x in argv[1:3]]
     file_name = argv[3]
     audio_name = argv[4]
     feature = extract(audio_name)
-    img = generateBrush(feature)
+    # img = generateBrush(feature)
+    img = powerSpectrum2Brush(feature["power_spec"])
     cv2.imwrite(file_name,img)
